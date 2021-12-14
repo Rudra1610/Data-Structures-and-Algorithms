969fe2182b39907440d1018fe9b897de8f0b91f0
Rudra Sakariya <rudrasakariya16@gmail.com>
2021-12-15 00:41:05 +0530

    Done

--- Selection-Sort.c
+++ Selection-Sort.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
-    
+    int n, min;
+    printf("Enter the number of elements ");
+    scanf("%d", &n);
+    int array[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the value");
+        scanf("%d", &array[i]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (array[j] < array[min])
+            {
+
+                min = j;
+                int temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
     return 0;
-
 }
